31d6ab5e3ebd80ace655894444e913e336a30b15
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-06-13 20:45:27 +0700

    feat: finish face recognition

--- api.py
+++ api.py
@@ -1,13 +1,7 @@
-from io import BytesIO
 from fastapi import FastAPI, Response, UploadFile, Form
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import face_recognition
-import numpy as np
-import shutil
-import os
-from database import SessionLocal
-from models import DtUser
 
 app = FastAPI()
 
@@ -22,71 +16,27 @@ app.add_middleware(
 KNOWN_FOLDER = "./known_faces"
 
 @app.post("/verify")
-async def verify_person(res: Response, name: str = Form(...), file: UploadFile = Form(...)):
-    # Save to postgres
-    db = SessionLocal()
-    user_img: str
-    try:    
-        record = db.query(DtUser).filter(DtUser.user_name == name).first()
+async def verify_person(res: Response, comparedImg: UploadFile = Form(...), refImg: UploadFile = Form(...)):
+    # Encode compared image
+    compared_img = face_recognition.load_image_file(comparedImg.file)
+    compared_encodings = face_recognition.face_encodings(compared_img)
 
-        if record:
-            if record.face_img:    
-                user_img = record.face_img
-            else:
-                res.status_code = 400
-                return {
-                    "message": "User hasn't uploaded reference image"
-                }
-        else:
-            res.status_code = 400
-            return {
-                "message": "User doesn't exist in database"
-            }
-    finally:
-        db.close()
+    # Encode reference image
+    ref_img = face_recognition.load_image_file(refImg.file)
+    ref_encodings = face_recognition.face_encodings(ref_img)
 
-    # Save the uploaded file temporarily
-    # temp_path = f"./temp_upload.jpg"
-    # with open(temp_path, "wb") as buffer:
-    #     shutil.copyfileobj(file.file, buffer)
-    user_img = BytesIO(user_img)
+    if not compared_encodings:
+        return JSONResponse(status_code=400, content={"detail": "No face detected in compared image."})
+    if not ref_encodings: 
+        return JSONResponse(status_code=400, content={"detail": "No face detected in reference image."})
 
-    # Encode uploaded face
-    unknown_img = face_recognition.load_image_file(user_img)
-    unknown_encodings = face_recognition.face_encodings(unknown_img)
+    ref_encoding = ref_encodings[0]
+    compared_encoding = compared_encodings[0]
 
-    if not unknown_encodings:
-        return JSONResponse(status_code=400, content={"detail": "No face detected in uploaded image."})
+    result = face_recognition.compare_faces([ref_encoding], compared_encoding, tolerance=0.5)
+    distance = face_recognition.face_distance([ref_encoding], compared_encoding)[0]
 
-    unknown_encoding = unknown_encodings[0]
-
-    # Load known images for that name
-    person_folder = os.path.join(KNOWN_FOLDER, name)
-    print(f"person folder: {person_folder}")
-    if not os.path.exists(person_folder):
-        return JSONResponse(status_code=404, content={"detail": "Person not found in DB."})
-
-    known_encodings = []
-    for filename in os.listdir(person_folder):
-        path = os.path.join(person_folder, filename)
-        print(f"path: {path}")
-        image = face_recognition.load_image_file(path)
-        encs = face_recognition.face_encodings(image)
-        if encs:
-            known_encodings.append(encs[0])
-
-    if not known_encodings:
-        return JSONResponse(status_code=500, content={"detail": "No valid face encodings for this person."})
-
-    # Average encoding
-    avg_encoding = np.mean(known_encodings, axis=0)
-
-    # Compare
-    result = face_recognition.compare_faces([avg_encoding], unknown_encoding, tolerance=0.5)
-    distance = face_recognition.face_distance([avg_encoding], unknown_encoding)[0]
     isMatch = bool(result[0])
-
-    os.remove(temp_path)
 
     # Response
     return {
--- database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Replace with your actual PostgreSQL credentials
-DATABASE_URL = "postgresql://postgres:aiface_123@128.199.119.151:5432/face_attendance"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
